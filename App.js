803001921eba8a5035bde08470f7d2e94e549d5f
Jordan Mack <jordan.mack37@gmail.com>
2025-03-15 16:09:44 -0400

    change title

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from './app/screens/LoginScreen';
-import MainPage from './app/screens/MainPage';
-import WelcomeScreen1 from './app/screens/WelcomeScreen'
-import WelcomeScreen2 from './app/screens/WelcomeScreen2';
-import WelcomeScreen3 from './app/screens/WelcomeScreen3';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./app/screens/LoginScreen";
+import MainPage from "./app/screens/MainPage";
+import WelcomeScreen1 from "./app/screens/WelcomeScreen";
+import WelcomeScreen2 from "./app/screens/WelcomeScreen2";
+import WelcomeScreen3 from "./app/screens/WelcomeScreen3";
 
 const Stack = createStackNavigator();
 
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Welcome1' component={WelcomeScreen1} />
+        <Stack.Screen name="Welcome to Fast Track" component={WelcomeScreen1} />
         <Stack.Screen name="Welcome2" component={WelcomeScreen2} />
         <Stack.Screen name="Welcome3" component={WelcomeScreen3} />
         <Stack.Screen name="Login" component={LoginScreen} />
