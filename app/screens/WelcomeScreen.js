5935e0d43ba7819cab8ce09e47fdc8d483de8e99
Jordan Mack <jordan.mack37@gmail.com>
2025-03-15 16:42:13 -0400

    fix zindex on welcome page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,131 +1,153 @@
-import { Text, StyleSheet, View, TouchableOpacity, Image, onPress } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  onPress,
+} from "react-native";
 import Emoji from "react-native-emoji";
 
 function WelcomeScreen1({ navigation }) {
-
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/Subtract.png')} style={styles.subtract}/>
-      <Image source={require('../assets/phoneintro.png')} style={styles.image} />
-      <View style={styles.imageContainer}>
-        
-      </View>
-      <View style={styles.textContainer}>
-        
-        <Text style={styles.textTitle}>Fast Track - Start Your Fasting Journey Today! <Emoji name="muscle" /></Text>
-        <Text style={styles.textP}>
-          Welcome to Fast Track, your all-in-one solution for intermittent fasting success. 
-          Let's get started on your journey to better health and well-being!
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FE3756",
+        zIndex: 1,
+        overflow: "hidden",
+      }}
+    >
+      <Image
+        source={require("../assets/Subtract.png")}
+        style={{
+          zIndex: 2,
+          position: "absolute",
+          bottom: -10,
+        }}
+      />
+      <Image
+        source={require("../assets/phoneintro.png")}
+        style={{
+          position: "absolute",
+          top: 20,
+          width: 530,
+          height: 530,
+          resizeMode: "contain",
+          marginBottom: 20,
+        }}
+      />
+      <View
+        style={{
+          flex: 2,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "#FFF",
+          flex: 3,
+          zIndex: 2,
+          position: "absolute",
+          bottom: 200,
+          borderTopLeftRadius: -50,
+          borderTopRightRadius: -50,
+          width: "90%",
+          height: 200,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 22,
+            fontWeight: "bold",
+            color: "#000",
+            textAlign: "center",
+            marginBottom: 20,
+            marginTop: 30,
+            zIndex: 3,
+            lineHeight: 30,
+          }}
+        >
+          Fast Track - Start Your Fasting Journey Today! <Emoji name="muscle" />
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            zIndex: 3,
+            paddingHorizontal: 10,
+            textAlign: "center",
+            lineHeight: 24,
+          }}
+        >
+          Welcome to Fast Track, your all-in-one solution for intermittent
+          fasting success. Let's get started on your journey to better health
+          and well-being!
         </Text>
       </View>
-      
-      <View style={styles.footerContainer}>
-      <Image source={require('../assets/layout-horizontal-1.png')} style={styles.horizontalLine}/>
-      <View style={styles.footerBtnContainer}>
-        <TouchableOpacity onPress={() => navigation.navigate('Main')} style={styles.skipBtn}>
-          <Text>Skip</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Welcome2')} style={styles.continueBtn}>
-          <Text>Continue</Text>
-        </TouchableOpacity>
+
+      <View
+        style={{
+          flex: 2,
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignContent: "center",
+          gap: 20,
+          zIndex: 5,
+        }}
+      >
+        <Image
+          source={require("../assets/layout-horizontal-1.png")}
+          style={{
+            zIndex: 5,
+            position: "absolulte",
+            bottom: 40,
+            backgroundColor: "#FFF",
+          }}
+        />
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: "50px",
+            paddingVertical: 20,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Main")}
+            style={{
+              backgroundColor: "#FE3756",
+              height: 50,
+              width: 120,
+              borderRadius: 50,
+              zIndex: 5,
+              bottom: 30,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>Skip</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Welcome2")}
+            style={{
+              backgroundColor: "#FE3756",
+              height: 50,
+              width: 120,
+              borderRadius: 50,
+              zIndex: 5,
+              bottom: 30,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>Continue</Text>
+          </TouchableOpacity>
         </View>
       </View>
-      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FE3756",
-    zIndex: 1,
-  },
-  imageContainer: {
-    flex: 2,
-  },
-  image: {
-    position: "absolute",
-    top: 20,
-    width: 530, 
-    height: 530,
-    resizeMode: "contain",
-    marginBottom: 20,
-  },
-  textContainer: {
-    backgroundColor: "#FFF",
-    flex: 3,
-    zIndex: 2,
-    position: "absolute",
-    bottom: 200,
-    borderTopLeftRadius: -50, 
-    borderTopRightRadius: -50,
-    width: "90%",
-    height: 200,
-  },
-  textTitle: {
-    fontSize: 22,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center",
-    marginBottom: 20,
-    marginTop: 30,
-    zIndex: 3,
-    lineHeight: 30,
-  },
-  textP: {
-    fontSize: 16,
-    zIndex: 3,
-    paddingHorizontal: 10,
-    textAlign: "center",
-    lineHeight: 24,
-  },
-  subtract: {
-    zIndex: 2,
-    position: "absolute",
-    bottom: -10,
-  },
-  horizontalLine: {
-    zIndex:5,
-    position: "absolulte",
-    bottom: 40,
-    backgroundColor:"#FFF"
-  },
-  footerContainer: {
-    flex: 5,
-    justifyContent: 'flex-end',
-    alignContent:'center',
-    flexDirection: "column",
-    gap: 20,
-  },
-  footerBtnContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: 50,
-    marginBottom: 20,
-  },
-  continueBtn: {
-    backgroundColor: "#FE3756",
-    height: 50,
-    width: 120,
-    borderRadius: 50,
-    zIndex: 5,
-    bottom: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  skipBtn: {
-    backgroundColor: "#FE3756",
-    height: 50,
-    width: 120,
-    borderRadius: 50,
-    zIndex: 5,
-    bottom: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
 
 export default WelcomeScreen1;
